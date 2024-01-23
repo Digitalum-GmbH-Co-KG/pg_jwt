26101a535b1748012106bb2d1e5791e5d5f64212
thomas.steffen <thomas.steffen@ang.de>
2024-01-23 16:08:09 +0100

    pg_jwt--1.0.0.sql: c-func name was wrong

--- pg_jwt--1.0.0.sql
+++ pg_jwt--1.0.0.sql
@@ -1,20 +1,20 @@
 DROP FUNCTION IF EXISTS jwt_verify(cert_pub_key text, jwt_token text, jwt_issuer text);
 
 CREATE OR REPLACE FUNCTION jwt_verify(cert_pub_key text, jwt_token text, jwt_issuer text) 
-	RETURNS text AS 'jwt_func.so', 'jwt_verify_rs256'
+	RETURNS text AS 'pg_jwt.so', 'jwt_verify_rs256'
 	LANGUAGE C STRICT;
 
 DROP FUNCTION IF EXISTS jwt_decode(jwt_token text);
 
 CREATE OR REPLACE FUNCTION jwt_decode(jwt_token text)
 	RETURNS text
-	AS 'jwt_func.so', 'jwt_decode'
+	AS 'pg_jwt.so', 'jwt_decode'
 	LANGUAGE C STRICT;
 
 DROP FUNCTION IF EXISTS jwt_create(jwt_priv_key text, jwt_data text);
 
 CREATE OR REPLACE FUNCTION jwt_create(jwt_priv_key text, jwt_data text)
         RETURNS text
-	AS 'jwt_func.so', 'jwt_create_rs256'
+	AS 'pg_jwt.so', 'jwt_create_rs256'
 	LANGUAGE C STRICT;
 
