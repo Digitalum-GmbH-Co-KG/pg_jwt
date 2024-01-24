07b928c8a3ed4f29e4598087497ec5f70fdba3c2
thomas.steffen <thomas.steffen@ang.de>
2024-01-24 08:24:50 +0100

    Replace library name with MODULE_PATHNAME parameter from vontrol file

--- pg_jwt--1.0.0.sql
+++ pg_jwt--1.0.0.sql
@@ -1,20 +1,20 @@
 DROP FUNCTION IF EXISTS jwt_verify(cert_pub_key text, jwt_token text, jwt_issuer text);
 
 CREATE OR REPLACE FUNCTION jwt_verify(cert_pub_key text, jwt_token text, jwt_issuer text) 
-	RETURNS text AS 'pg_jwt.so', 'jwt_verify_rs256'
-	LANGUAGE C STRICT;
+	RETURNS text AS 'MODULE_PATHNAME', 'jwt_verify_rs256'
+	IMMUTABLE STRICT LANGUAGE C PARALLEL SAFE;
 
 DROP FUNCTION IF EXISTS jwt_decode(jwt_token text);
 
 CREATE OR REPLACE FUNCTION jwt_decode(jwt_token text)
 	RETURNS text
-	AS 'pg_jwt.so', 'jwt_decode'
-	LANGUAGE C STRICT;
+	AS 'MODULE_PATHNAME', 'jwt_decode'
+	IMMUTABLE STRICT LANGUAGE C PARALLEL SAFE;
 
 DROP FUNCTION IF EXISTS jwt_create(jwt_priv_key text, jwt_data text);
 
 CREATE OR REPLACE FUNCTION jwt_create(jwt_priv_key text, jwt_data text)
         RETURNS text
-	AS 'pg_jwt.so', 'jwt_create_rs256'
-	LANGUAGE C STRICT;
+	AS 'MODULE_PATHNAME', 'jwt_create_rs256'
+	IMMUTABLE STRICT LANGUAGE C PARALLEL SAFE;
 
